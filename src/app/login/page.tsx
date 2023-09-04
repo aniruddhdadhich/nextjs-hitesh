6162c531086eaddf792a820c31d59eaafbd33828
aniruddhdadhich <aniruddhddh17@gmail.com>
2023-09-04 14:41:10 +0530

    nextjs 13 kickstart

---
+++ src/app/login/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import React, { useState } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation';
+import { Axios } from 'axios';
+
+const LoginPage = () => {
+  // user details
+  const [user,setUser] = useState({
+    email:"",
+    password:"",
+    username:""
+  })
+
+
+
+  return (
+    <h1 className='text-2xl text-green-400'>LoginPage</h1>
+  )
+}
+
+export default LoginPage
